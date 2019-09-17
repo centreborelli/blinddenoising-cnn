b3006fa0b00cdb2510d1af317332edf6dcbd9ade
Jérémy Anger <angerj.dev@gmail.com>
2019-09-17 10:34:17 +0200

    move models to models/

--- train.sh
+++ train.sh
@@ -3,23 +3,23 @@ sigma=20
 
 # (noisy vis, ir) -> noisy vis
 args="--invis fusion$sigma --inir fusion --outvis fusion$sigma --outir fusion"
-out=results/n2n_visir_sigma$sigma/
+out=models/n2n_visir_sigma$sigma/
 rm -r $out; mkdir -p $out
 python3 main.py train . --inchannels 2 --outchannels 1 --lr 1e-3 --saveto $out $args
 
 # noisy vis -> noisy vis
-out=results/n2n_vis_sigma$sigma/
+out=models/n2n_vis_sigma$sigma/
 rm -r $out; mkdir -p $out
 python3 main.py train . --inchannels 1 --outchannels 1 --lr 1e-3 --saveto $out $args
 
 # noisy ir -> noisy ir
 args="--invis fusion --inir fusion$sigma --outvis fusion --outir fusion$sigma"
-out=results/n2n_ir_sigma$sigma/
+out=models/n2n_ir_sigma$sigma/
 rm -r $out; mkdir -p $out
 python3 main.py train . --inchannels 1 --outchannels 1 --lr 1e-3 --saveto $out --predict ir $args
 
 # (noisy ir, vis) -> noisy ir
-out=results/n2n_irvis_sigma$sigma/
+out=models/n2n_irvis_sigma$sigma/
 rm -r $out; mkdir -p $out
 python3 main.py train . --inchannels 2 --outchannels 1 --lr 1e-3 --saveto $out --predict ir $args
 
